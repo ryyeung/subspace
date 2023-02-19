330bdcdcbabffdabebd5231d5fa1e3a915a50260
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2023-02-19 12:14:00 +0800

    Modify core-eth-relay runtime
    - Change the crate name, spec name and author.
    - Add the domain specific pallets into the runtime.

--- domains/runtime/core-eth-relay/src/runtime.rs
+++ domains/runtime/core-eth-relay/src/runtime.rs
@@ -79,8 +79,8 @@ impl_opaque_keys! {
 
 #[sp_version::runtime_version]
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-    spec_name: create_runtime_str!("subspace-core-payments-domain"),
-    impl_name: create_runtime_str!("subspace-core-payments-domain"),
+    spec_name: create_runtime_str!("subspace-core-eth-relay-domain"),
+    impl_name: create_runtime_str!("subspace-core-eth-relay-domain"),
     authoring_version: 0,
     spec_version: 0,
     impl_version: 0,
